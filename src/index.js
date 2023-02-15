d2790f5d5f76ced96406d0cb2d26bfa97ce48476
minkman <lomick2090@gmail.com>
2023-02-15 14:02:10 -0600

    Create initial dom manipulation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { player } from './player';
 
 
 const game = document.querySelector('.game');
+let gameb1;
+let gameb2;
 
 function gameSetUpPage() {
     let nameInput = document.createElement('input');
@@ -18,7 +20,45 @@ function gameSetUpPage() {
     submit.innerHTML = 'Start Game!';
     
     game.appendChild(submit);
-    game.classList.add('setup')
+    game.classList.add('setup');
+}
+
+function gamePage() {
+    while (game.firstChild) {
+        game.firstChild.remove();
+    }
+
+    game.classList.remove('setup');
+
+    gameb1 = document.createElement('div');
+    gameb2 = document.createElement('div');
+
+    gameb1.className = 'gameboard';
+    gameb2.className = 'gameboard';
+
+    for (let i = 1; i<= 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+            let newsquare = document.createElement('div');
+            newsquare.x = i;
+            newsquare.y = j;
+            newsquare.className = 'square'
+            gameb1.appendChild(newsquare);
+        }
+    }
+
+    for (let i = 1; i<= 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+            let newsquare = document.createElement('div');
+            newsquare.x = i;
+            newsquare.y = j;
+            newsquare.className = 'square'
+            gameb2.appendChild(newsquare);
+        }
+    }
+
+    game.appendChild(gameb1);
+    game.appendChild(gameb2);
+    
 }
 
 function startGame(name) {
@@ -27,8 +67,10 @@ function startGame(name) {
     let gameboard1 = gameboard();
     let gameboard2 = gameboard();
 
-    player1.playerboard = gameboard1
+    player1.playerboard = gameboard1;
     computer.playerboard = gameboard2;
+
+    gamePage(name);
 }
 
 gameSetUpPage();
